69fbafd29fe6edf826d1381db92496ab5518049f
ErenVarli9 <eren.varli@epitech.eu>
2025-03-31 13:13:25 +0200

    [EDIT] - Add providers for stores

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,17 +3,23 @@ import {StyleSheet, SafeAreaView} from 'react-native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { PageHome } from '@/pages/PageHome';
 import { PageBoards } from '@/pages/PageBoards';
+import { PageWorkpaces } from '@/pages/PageWorspaces';
+import { Provider } from 'react-redux';
+import store from '@/store';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
 
   return (
+    <Provider store={store}>
     <SafeAreaView style={styles.container}>
        <Stack.Navigator screenOptions={{ headerShown: false }}>
        <Stack.Screen name="PageHome" component={PageHome} /> 
        <Stack.Screen name="PageBoards" component={PageBoards} /> 
+       <Stack.Screen name="PageWorkspaces" component={PageWorkpaces} /> 
        </Stack.Navigator>
     </SafeAreaView>
+    </Provider>
   );
 };
 
