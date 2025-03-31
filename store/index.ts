be536348529d09e193bf53de8f3cad3daaf12bf1
ErenVarli9 <eren.varli@epitech.eu>
2025-03-31 14:34:41 +0200

    [edit] - add card slice

--- store/index.ts
+++ store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import organizationSlice from "@/store/slices/organizationSlice";
 import boardSlice from "@/store/slices/boardSlice";
 import listSlice from "@/store/slices/listSlice";
+import cardSlice from "@/store/slices/cardSlice";
 
 const store = configureStore({
   reducer: {
     organization: organizationSlice,
     board: boardSlice,
     list: listSlice,
+    card: cardSlice,
   },
 });
 
