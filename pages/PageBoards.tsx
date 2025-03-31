0b71183f16dd096f656eb8e97ac3b58ac3d491bb
ErenVarli9 <eren.varli@epitech.eu>
2025-03-31 13:53:26 +0200

    [edit] - redirect to lists

--- pages/PageBoards.tsx
+++ pages/PageBoards.tsx
@@ -1,13 +1,19 @@
 import { Cards } from "@/components/trello/card";
-import store from "@/store";
+import store, { RootState } from "@/store";
+import { setBoardData } from "@/store/slices/boardSlice";
 import { createBoard, createBoardByOrganizationId, getBoards } from "@/utils/trello/boards";
+import { useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Alert } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 import { Toast } from "toastify-react-native";
 
 export const PageBoards = () => {
   const [boards, setBoards] = useState<any[]>();
   const [organizationId, setOrganizationId] = useState(store.getState().organization.data.id);
+  const board = useSelector((state: RootState) => state.board.data);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
 
     const fetchBoards = async () => {
       const responseData = await getBoards(organizationId);
@@ -19,8 +25,14 @@ export const PageBoards = () => {
         if (name) await createBoardByOrganizationId(name, organizationId) 
           ? Toast.success("Board created.") : Toast.error("Error during board creation.")
       });
+      navigation.navigate("PageLists" as never)
     }
     
+    const handleSelectBoard = (boardId: string) => {
+      dispatch(setBoardData({ ...board, id: boardId }))
+      navigation.navigate("PageLists" as never)
+    };
+
     useEffect(() => {
       fetchBoards();
     }, []);
@@ -29,7 +41,7 @@ export const PageBoards = () => {
     <View style={styles.container}>
       <Text style={styles.text}>Liste des tableaux</Text>
       {boards?.map((board, index) => (
-        <Cards title={board.name} key={index}/>
+        <Cards title={board.name} key={index} onPress={() => handleSelectBoard(board.id)}/>
       ))}
       <Cards title="Create a board" onPress={handleCreateBoard}/>
     </View>
