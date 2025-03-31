132b4fe6b96ed30a4f74e474496292fad157e2ff
ErenVarli9 <eren.varli@epitech.eu>
2025-03-31 13:14:25 +0200

    [edit] - page workspace

---
+++ pages/PageWorspaces.tsx
@@ -0,0 +1,59 @@
+import { Cards } from "@/components/trello/card";
+import { RootState } from "@/store";
+import { setOrganizationData } from "@/store/slices/organizationSlice";
+import { getOrganization } from "@/utils/trello/organizations";
+import { useNavigation } from "expo-router";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useDispatch, useSelector } from 'react-redux'
+
+export const PageWorkpaces = () => {
+    const [workspaces, setWorkspaces] = useState<any[]>();
+    const organization = useSelector((state: RootState) => state.organization.data);
+    const navigation = useNavigation();
+    const dispatch = useDispatch()
+
+
+  const fetchOrganizations = async () => {
+    const responseData = await getOrganization();
+    if (responseData) setWorkspaces(responseData);
+  };
+
+  const handleStoreOrganizationDataRedirectBoard = (workspaceId: string) => {
+    dispatch(setOrganizationData({
+        ...organization,
+        id: workspaceId,
+    }))
+    navigation.navigate("PageBoards" as never)
+  }
+
+  useEffect(() => {
+    fetchOrganizations();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My workspaces</Text>
+      <View>
+        {workspaces?.map((workspace, index) => (
+          <Cards title={workspace.displayName} key={index} onPress={() => handleStoreOrganizationDataRedirectBoard(workspace.id)}/>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#222831",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: "700",
+    color: "#ffffff",
+    textAlign: "center",
+  },
+});
