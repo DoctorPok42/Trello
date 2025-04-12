8b736ec7db2a6f8124986ed30e5eb65f12aad3d7
ErenVarli9 <eren.varli@epitech.eu>
2025-04-12 22:50:23 +0200

    Update tab title and icon for Workspaces in TabLayout component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,9 +32,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "Workspaces",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+            <IconSymbol size={28} name="table.fill" color={color} />
           ),
         }}
       />
