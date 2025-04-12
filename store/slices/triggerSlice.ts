0d2f58adab4a04e674fc5a3a3185a7f21dcd15b4
ErenVarli9 <eren.varli@epitech.eu>
2025-04-13 01:08:31 +0200

    [EDIT] - Refactor triggerSlice to consolidate triggers into activeTrigger and remove unused triggers

--- store/slices/triggerSlice.ts
+++ store/slices/triggerSlice.ts
@@ -3,24 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const triggerSlice = createSlice({
   name: "trigger",
   initialState: {
-    updateCardTrigger: false,
-    deleteCardTrigger: false,
-    updateOrganizationTrigger: false,
-
+    activeTrigger: false,
   },
   reducers: {
-    updateCardTrigger: (state, action) => {
-      state.updateCardTrigger = action.payload;
-    },
-    deleteCardTrigger: (state, action) => {
-      state.deleteCardTrigger = action.payload;
-    },
-    updateOrganizationTrigger: (state, action) => {
-      state.updateOrganizationTrigger = action.payload;
+    activeTrigger: (state, action) => {
+      state.activeTrigger = action.payload;
     }
   },
 });
 
-export const { deleteCardTrigger, updateCardTrigger, updateOrganizationTrigger } = triggerSlice.actions;
+export const {
+  activeTrigger,
+} = triggerSlice.actions;
 
 export default triggerSlice.reducer;
