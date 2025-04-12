3c7c6bd079126f5fc26b2985af89abf56517ab0c
ErenVarli9 <eren.varli@epitech.eu>
2025-04-13 00:56:32 +0200

    [EDIT] - Refactor error handling in board-related functions and add deleteBoard and updateBoard functionalities

--- utils/trello/boards.ts
+++ utils/trello/boards.ts
@@ -18,7 +18,10 @@ export const createBoard = async (name: string) => {
   }
 };
 
-export const createBoardByOrganizationId = async (name: string, idOrganization: string ) => {
+export const createBoardByOrganizationId = async (
+  name: string,
+  idOrganization: string
+) => {
   const url = `https://api.trello.com/1/boards/?name=${name}&idOrganization=${idOrganization}&key=${APIKey}&token=${APIToken}`;
   const params = { method: "POST" };
 
@@ -27,7 +30,7 @@ export const createBoardByOrganizationId = async (name: string, idOrganization: 
     if (response.status === 200) return true;
     else return false;
   } catch (err) {
-    console.log("Erreur [A1] : " + err);
+    console.log("Erreur [BM1] : " + err);
     return false;
   }
 };
@@ -41,7 +44,7 @@ export const getBoardsByID = async (organizationId: string) => {
     const responseData = await response.json();
     return responseData;
   } catch (err) {
-    return "Erreur [A2] : " + err;
+    return "Erreur [BM2] : " + err;
   }
 };
 
@@ -54,6 +57,33 @@ export const getBoards = async () => {
     const responseData = await response.json();
     return responseData;
   } catch (err) {
-    return "Erreur [A3] : " + err;
+    return "Erreur [BM3] : " + err;
+  }
+};
+
+export const deleteBoard = async (boardID: string) => {
+  const url = `https://api.trello.com/1/boards/${boardID}?key=${APIKey}&token=${APIToken}`;
+  const params = { method: "DELETE", headers: { Accept: "application/json" } };
+
+  try {
+    const response = await fetch(url, params);
+    const responseData = await response.json();
+    if (response.status === 200) return responseData;
+  } catch (err) {
+    console.log("[BM4] - ", err);
+    return false;
+  }
+};
+export const updateBoard = async (boardID: string, name?: string) => {
+  const url = `https://api.trello.com/1/boards/${boardID}?key=${APIKey}&token=${APIToken}&name=${name}`;
+  const params = { method: "PUT", headers: { Accept: "application/json" } };
+
+  try {
+    const response = await fetch(url, params);
+    if (response.status === 200) return true;
+    else return false;
+  } catch (err) {
+    console.log("[BM5] - ", err);
+    return false;
   }
 };
